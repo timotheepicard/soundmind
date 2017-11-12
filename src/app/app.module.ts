b3630b48eea6809e47b8ac6b66ada5bace4450f8
timotheepicard <ptim64@gmail.com>
2017-11-12 22:16:58 +0100

    ajout des lib js bootstrap, tether et jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
+import { SignupComponent } from './signup/signup/signup.component';
+import { NavComponent } from './core/nav/nav/nav.component';
+import { FooterComponent } from './core/footer/footer/footer.component';
+import { Error404Component } from './core/error404/error-404/error-404.component';
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NavComponent,
+    FooterComponent,
+    Error404Component
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // debug only
+    ),
     BrowserModule
   ],
   providers: [],
